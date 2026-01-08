0132bb924e29d15a92748f77f5e87cb6ede221ef
papor_33 <kekuhaleksandr@gmail.com>
2026-01-09 00:24:10 +0200

    feat: base template

--- crm/views.py
+++ crm/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from django.db.models import Q
+from .models import Client, Deal
 
-# Create your views here.
+
+class ClientListView(ListView):
+    model = Client
+    template_name = 'crm/client_list.html'
+    context_object_name = 'clients'
+    paginate_by = 10  # Пагинация по 10 штук
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        query = self.request.GET.get('q')
+
+        # Логика поиска по имени, телефону или email
+        if query:
+            queryset = queryset.filter(
+                Q(name__icontains=query) |
+                Q(phone__icontains=query) |
+                Q(email__icontains=query)
+            )
+        return queryset
